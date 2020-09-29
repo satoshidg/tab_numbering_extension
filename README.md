e6a8854868116b38af7a9db54b2995ab1b1e9d96
satoshidg <satoshidg3104@gmail.com>
2020-09-29 00:57:05 -0700

    refactoring and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Tab Numbering Extension
 
-This repo is a Chrome/Edge browser extension that number each tab for easier use of shortcut (ex. cmd + 4).
+This repo is for a Chrome/Edge browser extension that number each tab for easier use of shortcut (ex. cmd + 4).
+
+Any feedback is welcome!!
--- background.js
+++ background.js
@@ -11,8 +11,32 @@
 
 const tabNumberRegEx = /{[1-9]}/;
 
-function getTabIsNumbered(title) {
+function tabIsNumbered(title) {
     return tabNumberRegEx.test(title);
+}
+
+function numberTabs(tabs) {
+    const lastTabIndex = tabs.length - 1;
+
+    for (let index = 0; index < tabs.length; index++) {
+        let tabNumber = index + 1; // Actual numbering of the tab
+        let tabName = tabs[index].title;
+
+        // Remove all existing numbering
+        if (tabIsNumbered(tabName)) {
+            tabName = tabName.substr(3);
+        }
+
+        if (index < 8) {
+            tabName = `{${tabNumber}} ${tabName}`;
+        } else if (index == lastTabIndex) {
+            tabName = `{9} ${tabName}`;
+        }
+
+        let script = `document.title = "${tabName}"`;
+
+        chrome.tabs.executeScript(tabs[index].id, {code: script});
+    }
 }
 
 function numberAllTabs() {
@@ -20,35 +44,6 @@ function numberAllTabs() {
         numberTabs(tabs);
     });
 }
-
-function numberTabs(tabs) {
-    let index = 0;
-    let lastTabIndex = tabs.length - 1;
-
-    for (index = 0; index < tabs.length; index++) {
-        let tabNumber = index + 1;
-        let currentTabName = tabs[index].title;
-        let script = "";
-
-        if (getTabIsNumbered(currentTabName)) {
-            currentTabName = currentTabName.substr(3);
-        }
-
-        if (index < 8) {
-            let tabName = "{" + tabNumber + "} " + currentTabName;
-            script = "document.title = '" + tabName + "'";
-        } else if (index == lastTabIndex) {
-            let tabName = "{9} " + currentTabName;
-            script = "document.title = '" + tabName + "'";
-        } else {
-            // When a tab is between 8 and last, make sure to delete the numbering if it exists
-            script = "document.title = '" + currentTabName + "'";
-        }
-        chrome.tabs.executeScript(tabs[index].id, {code: script});
-    }
-}
-
-
 
 chrome.tabs.onCreated.addListener(function() {
     numberAllTabs();
